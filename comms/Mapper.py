ad76cccfb2b4a70e1d94c41e87953a2130fb6b9b
paritoshpr <pramanan3@MacBook-Air-4.attlocal.net>
2023-04-02 20:50:10 -0500

    comms support added

---
+++ comms/Mapper.py
@@ -0,0 +1,217 @@
+from RedisComm import RedisComm
+
+import logging
+
+
+class Mapper(RedisComm):
+    """
+    Mapper Scarlet maps keys to values. Values could be anything such as model parameters, arrays etc.
+    This Mapper inherits from multiple classes. However, at run time depending on the mode of the scarlet declaration,
+    the classes are initialized.
+
+    Attributes
+    ----------
+
+    key : str
+        A unique string used to identify edge device
+
+    Methods
+    -------
+
+    * `refresh()`
+        refresh contract details
+
+    * `_registerNewKey(key)`
+        registers new key by calling the corresponding underlying contract function
+
+    * `Map(modelLocal,key)`
+        maps model parameters to a given key
+
+    * `AllGather(modelLocal)`
+        Performs an AllGather operation in which all the key-value pairs are obtained.
+
+    * `Reduce(modelLocal,op)`
+        Performs a Reduce operation which comprises of an AllGather followed by an operation on all the values
+        obtained thus far.
+
+    * `resetAll(modelLocal)`
+        Resets all the key-value pairs
+    """
+
+    def __init__(self, scarletName):
+
+        RedisComm.__init__(self,scarletName)
+        self.key = str(self.address)
+
+        self.refresh()
+
+    def refresh(self):
+        """refresh contract details"""
+        self.loadContract()
+
+        self.key = str(self.address)
+
+        if not self._registerNewKey(self.key):
+            logging.critical("Key: {} already being used".format(self.key))
+
+    def _registerNewKey(self, key):
+        """registers new key by calling the corresponding underlying contract function
+
+        Attributes
+        ----------
+        key : string
+            value of key to be registered
+
+        """
+        keyRegisterSuccess = self.contract.registerNewKey(key)
+        return keyRegisterSuccess
+
+    def Map(self, modelLocal, key):
+
+        """maps model parameters to a given key
+
+        Attributes
+        ----------
+        modelLocal : numpy array
+            the content concerning modelLocal
+
+        key : string
+            value of key to be registered
+
+        Returns
+        -------
+
+        successChunksList : list
+            concerns the chunks which were successfully mapped
+
+        status : boolean
+            status of the Map operation
+
+        exception : Exception
+            exception if any else it will be None
+
+        """
+
+        successChunksList = []
+        try:
+
+            self.refresh()
+            successChunksList = self.Push(modelLocal, key, [])
+        except Exception as exception:
+            logging.error("{}.Map failed", self.scarletName)
+            return successChunksList, False, exception
+        return successChunksList, True, None
+
+    def AllGather(self, modelLocal):
+        """Performs an AllGather operation in which all the key-value pairs are obtained from the decentralized
+        infrastructure.
+
+        Attributes
+        ----------
+        modelLocal : numpy array
+            the content concerning modelLocal
+
+        Returns
+        -------
+
+        allgather_dict : dict
+            the dictionary containing all key value pairs
+
+        status : boolean
+            status of the Map operation
+
+        exception : Exception
+            exception if any else it will be None
+
+        """
+        allgather_dict = {}
+        try:
+
+            self.refresh()
+
+            mapperLength = self.contract.getMapperLength()
+            for key_index in range(int(mapperLength)):
+                key = self.contract.getKey(key_index)
+                modelOut, status = self.Pull(modelLocal, key)
+                if not status:
+                    logging.error(
+                        "{}.AllGather.Pull failed wfor key :{}".format(
+                            self.scarletName, key
+                        )
+                    )
+                allgather_dict[key] = modelOut
+
+            return allgather_dict, True, None
+
+        except Exception as exception:
+            logging.error(
+                "{}.AllGather failed with exception {}".format(
+                    self.scarletName, exception
+                )
+            )
+            return allgather_dict, False, exception
+
+    def Reduce(self, modelLocal, op):
+        """Performs a Reduce operation which comprises of an AllGather followed by an operation on all the values
+        obtained thus far. The choice of operations is SUM,MAX,MIN,MULT. In case of MAX,MIN and MULT it will be an
+        element wise operation.
+
+        Attributes
+        ----------
+        modelLocal : numpy array
+            the content concerning modelLocal
+
+        op : operation
+            any one of the 4 operations SUM,MAX,MIN,MULT
+
+        Returns
+        -------
+
+        sumV : numpy array
+            final value after carrying out the operation sequentially on all values.
+
+        status : boolean
+            status of the Map operation
+
+        exception : Exception
+            exception if any else it will be None
+
+        """
+        sumV = modelLocal
+        allgather_dict, status, exception = self.AllGather(modelLocal)
+        if status:
+            for key in allgather_dict.keys():
+                sumV = self.performOperation(allgather_dict[key], sumV, op)
+            return sumV, status, None
+        else:
+            return sumV, status, exception
+
+    def resetAll(self, modelLocal):
+        """Resets all the key-value pairs
+
+        Attributes
+        ----------
+        modelLocal : numpy array
+            the content concerning modelLocal
+
+        Returns
+        -------
+
+        successChunksList : list
+            concerns the chunks which were successfully reset
+
+        exception : Exception
+            exception if any else it will be None
+
+        """
+        successChunksList = []
+        try:
+            self.refresh()
+            mapperLength = self.contract.getMapperLength()
+            for key_index in range(int(mapperLength)):
+                key = self.contract.getKey(key_index)
+                successChunksList = self.Push(modelLocal, key)
+        except Exception as exception:
+            logging.error("{}.resetAll failed", self.scarletName)
+            return successChunksList, exception
+        return successChunksList, None
---
+++ comms/RedisContract.py
@@ -0,0 +1,316 @@
+import logging, redis,time,os
+
+class RedisContract:
+    """
+    Base class for all types of Contracts. Contains all Contract attributes
+
+    Attributes
+    ----------
+    key_list : list
+        list of keys
+
+    Methods
+    -------
+
+    * `load()`
+        Loads contract details from redis. Only done if debug is True.
+
+    * `registerNewKey(key)`
+        registers new key on contract
+
+    * `loadRedis()`
+        Connect to redis given redisDBHost,redisDBPort and redisDBPwd
+
+    * `setChunk(key, chunk, chunk_content, address)`
+        Set the chunk for a particular key
+
+    * `checkChunkExists(key, chunk)`
+        Check if the chunk exists for a particular key
+
+    * `getChunk(key, chunk)`
+        Get chunk for a particular key
+
+    * `getChunkUpdater(key, chunk)`
+        Get the updater (address) of chunk for a particular key
+
+    * `getLastUpdateTime(key, chunk)`
+        Get the last updated time of chunk for a particular key
+
+    * `getMapperLength()`
+        gets mapper length, i.e. number of unique keys on the mapper. applicable only in case of Mapper scarlets.
+
+    * `getKey()`
+        if given an index key_index, retrieves the key at that location. applicable in case of Mapper scarlets.
+
+    """
+
+    def __init__(self):
+
+        self.key_list = []
+
+        self.load()
+
+    def load(self):
+
+        """
+        Loads contract details from redis. Only done if debug is True.
+        """
+
+
+        if "REDIS_DB_HOST" not in os.environ.keys():
+            raise Exception("REDIS_DB_HOST not set in os.environ")
+
+        if "REDIS_DB_PORT" not in os.environ.keys():
+            raise Exception("REDIS_DB_PORT not set in os.environ")
+
+        if "REDIS_DB_PWD" not in os.environ.keys():
+            raise Exception("REDIS_DB_PWD not set in os.environ")
+
+        self.redisDBHost = os.environ["REDIS_DB_HOST"]
+        self.redisDBPort = os.environ["REDIS_DB_PORT"]
+        self.redisDBPwd = os.environ["REDIS_DB_PWD"]
+
+    def registerNewKey(self, key):
+
+        """
+        registers new key on contract
+
+        Parameters
+        ----------
+        key : string
+            the key to be registered
+
+        Returns
+        -------
+
+        ret_val : boolean
+            represents whether the register method was successful on the SmartContract
+
+        """
+
+        key = str(key)
+        r, status, exception = self.loadRedis()
+        if status:
+            if not r.exists(self.contractName + "_key-value"+":"+key):
+                r.set(self.contractName + "_key-value"+":"+key,"exists")
+                return True
+            else:
+                return True
+
+        return False
+
+
+    def loadRedis(self):
+        """
+        Connect to redis given redisDBHost,redisDBPort and redisDBPwd
+
+        Returns
+        -------
+
+        r : redis object
+            the object that can connect to redis
+
+        success : boolean
+            flag that indicates whether the connect succeeded
+
+        exception : Exception
+            the exception that is thrown in case of an error
+
+        """
+        try:
+            if self.debug:
+                logging.info(self.redisDBHost,self.redisDBPort,self.redisDBPwd)
+
+            r = redis.StrictRedis(host=self.redisDBHost, port=int(self.redisDBPort),
+                                  password=self.redisDBPwd)
+            r.ping()
+            return r,True,None
+
+        except Exception as exception:
+            logging.error("could not connect to redis due to exception {}".format(exception))
+            return None,False,exception
+
+
+    def setChunk(self, key, chunk, chunk_content, address):
+        """
+        Set the chunk for a particular key
+
+        Parameters
+        ----------
+
+        key : string
+           the key id
+
+        chunk : int
+            the chunk id to be set
+
+        chunk_content : string
+            binary string that represents the chunk content
+
+        address : string
+            the address of the agent setting the string
+
+
+        Returns
+        -------
+
+        ret_val : boolean
+            flag that represents whether the set operation was successful or not
+
+        exception : Exception
+            Exception thrown in case of an error, None in case of no exception
+        """
+
+
+        r, status, exception = self.loadRedis()
+        if status:
+            r.hmset(self.contractName+"_key-value"+":"+str(key)+":"+str(chunk),{
+                                                                                    "updater":address,
+                                                                                    "content":chunk_content,
+                                                                                    "lastUpdatedTime":time.time()
+                                                                                })
+            return True,None
+        else:
+            return status,exception
+
+
+    def checkChunkExists(self, key, chunk):
+        """
+        Check if the chunk exists for a particular key
+
+        Parameters
+        ----------
+
+        key : string
+           the key id
+
+        chunk : int
+            the chunk id to be set
+
+        Returns
+        -------
+
+        ret_val : boolean
+            flag representing the existence of the flag
+        """
+
+
+        r, status, exception = self.loadRedis()
+        if status:
+            if r.exists(self.contractName+"_key-value"+":"+str(key)+":"+str(chunk)):
+                return True
+        return False
+
+
+    def getChunk(self,key, chunk):
+        """
+        Get chunk for a particular key
+
+        Parameters
+        ----------
+
+        key : string
+           the key id
+
+        chunk : int
+            the chunk id to be set
+
+        Returns
+        -------
+
+        chunk_content : string
+            binary string representing the chunk content, defaults to empty string in case of failure
+
+        """
+
+        r, status, exception = self.loadRedis()
+
+        if status:
+            if r.exists(self.contractName+"_key-value"+":"+str(key)+":"+str(chunk)):
+                chunkDict = r.hgetall(self.contractName+"_key-value"+":"+str(key)+":"+str(chunk))
+                return chunkDict[b'content']
+        return b''
+
+
+    def getChunkUpdater(self,key, chunk):
+        """
+        Get the updater (address) of chunk for a particular key
+
+        Parameters
+        ----------
+
+        key : string
+           the key id
+
+        chunk : int
+            the chunk id to be set
+
+        Returns
+        -------
+
+        chunk_updater : string
+            address of the last updater, None in case of failure
+
+        """
+
+        r, status, exception = self.loadRedis()
+        if status:
+            if r.exists(self.contractName + "_key-value" + ":" + str(key) + ":" + str(chunk)):
+                chunkDict = r.hgetall(self.contractName + "_key-value" + ":" + str(key) + ":" + str(chunk))
+                return chunkDict[b'updater']
+        return None
+
+
+    def getLastUpdateTime(self, key, chunk):
+        """
+        Get the last updated time of chunk for a particular key
+
+        Parameters
+        ----------
+
+        key : string
+           the key id
+
+        chunk : int
+            the chunk id to be set
+
+        Returns
+        -------
+
+        lastUpdatedTime : string
+            time of the last update, empty string in case of failure
+
+        """
+        r, status, exception = self.loadRedis()
+        if status:
+            if r.exists(self.contractName + "_key-value" + ":" + str(key) + ":" + str(chunk)):
+                chunkDict = r.hgetall(self.contractName + "_key-value" + ":" + str(key) + ":" + str(chunk))
+                return chunkDict[b'lastUpdatedTime'], None
+        return ""
+
+
+    def getKeysLength(self):
+
+        """
+        gets mapper length, i.e. number of unique keys on the mapper. applicable only in case of Mapper scarlets.
+        """
+
+        r, status, exception = self.loadRedis()
+
+        if status:
+
+            comprehensive_keys_list = r.keys(self.contractName + "_key-value:*")
+
+            self.key_list = [key.decode("utf-8").split(":")[1] for key in comprehensive_keys_list]
+            if not len(self.key_list):
+                logging.warning("getMapperLength yielded 0 keys for mapper:{}".format(self.contractName))
+            return len(self.key_list)
+
+        else:
+            return 0
+
+    def getKey(self,key_index):
+        """
+        if given an index key_index, retrieves the key at that location. applicable in case of Mapper scarlets.
+        """
+        return self.key_list[key_index]
