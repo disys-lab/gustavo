98e8723c654ec01419983272529e061a20e4a871
paritosh ramanan <paritosh.ramanan@okstate.edu>
2024-12-09 11:54:01 -0600

    build monitoring and manager #bump #linuxbuild #macbuild

--- gustavo/Home.py
+++ gustavo/Home.py
@@ -16,13 +16,6 @@ from gustavo.pages.config.SyncerConfig import SyncerConfig, refresh_registry, ch
 from gustavo.src.Composer import Composer
 import socket,os
 
-
-
-    # if check == 0:
-    #     print("Port is open")
-    # else:
-    #     print("Port is not open")
-
 if "visibility" not in st.session_state:
     st.session_state.visibility = "visible"
     st.session_state.disabled = False
@@ -33,10 +26,6 @@ if "registry_dict_list" not in st.session_state:
 with st.expander("Platform Config", expanded=False):
     pc = PlatformConfig()
     pc.platform()
-
-# with st.expander("Syncer Config", expanded=False):
-#     sy = SyncerConfig()
-#     sy.syncer()
 
 with st.expander("Registry List", expanded = False):
     registry_container = st.container()
