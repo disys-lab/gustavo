47eb8e649baecca6b6dff2543af191e360fd7e6d
paritosh ramanan <paritosh.ramanan@okstate.edu>
2025-02-05 18:14:49 -0600

    resolved environment variables data editor bugs in App Creation #bump #patch #linuxbuild #macbuild

--- builder.py
+++ builder.py
@@ -54,6 +54,16 @@ if gitRepo:
             ]
         )
 
+        subprocess.run(
+            [
+                "curl",
+                "-F",
+                "package=@" + str(packageInfo),
+                f"{GEMFURY_TOKEN2}",
+            ],
+            capture_output=True
+        )
+
         os.chdir(currentDir)
 else:
     print("provide Git repo details")
