f48255089d370c209608e2fbc1fbd878fcc5ec32
paritosh ramanan <paritosh.ramanan@okstate.edu>
2026-03-05 23:20:07 -0600

    #bump #patch #linuxbuild #macbuild #dockerbuild introduction of nextjs and fastapi method of UI

--- gustavo-ui/middleware.ts
+++ gustavo-ui/middleware.ts
@@ -30,5 +30,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/((?!_next/static|_next/image|favicon.ico|api/).*)"],
 };
