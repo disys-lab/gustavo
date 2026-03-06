2f3f6acaf0f03b74313cb2daf003954c3ecdfb48
paritosh ramanan <paritosh.ramanan@okstate.edu>
2026-03-05 22:37:42 -0600

    #bump #patch #linuxbuild #macbuild #dockerbuild introduction of nextjs and fastapi method of UI

--- gustavo-ui/next.config.mjs
+++ gustavo-ui/next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
-const FASTAPI_URL = process.env.FASTAPI_URL || "http://localhost:8000";
+const FASTAPI_URL = process.env.FASTAPI_URL || "http://127.0.0.1:8000";
 
 const nextConfig = {
   output: "standalone",
+  images: { unoptimized: true },
   async rewrites() {
     return [
       {
